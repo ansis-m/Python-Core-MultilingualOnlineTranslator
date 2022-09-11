4879f842573accd1d58eed9c5ab80adebd47dca5
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-11 20:03:09 +0300

    finished stage 4

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -2,45 +2,79 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://context.reverso.net/translation/"
-ef = "english-french/"
-fe = "french-english/"
+
+languages = ["placeholder",
+            "Arabic",
+             "German",
+             "English",
+             "Spanish",
+             "French",
+             "Hebrew",
+             "Japanese",
+             "Dutch",
+             "Polish",
+             "Portuguese",
+             "Romanian",
+             "Russian",
+             "Turkish"]
 
 
-def display_results(page, language):
+def display_results(page, i, j):
     soup = BeautifulSoup(page.content, 'html.parser')
     words = soup.find_all('span', {'class': 'display-term'})
-    print("200 OK")
-    print("\n{} translations:".format("French" if language == "fr" else "English"))
+
+    print("\n{} translations:".format(languages[j]))
     for w in words:
         print(w.text)
 
-    print("\n{} examples:".format("French" if language == "fr" else "English"))
+    print("\n{} examples:".format(languages[j]))
 
     sentences = soup.find('section', {'id': 'examples-content'}).findAll('span', {'class': 'text'})
     i = 1
     for s in sentences:
-        print(s.text.strip())
+        print(s.text.strip(), end="")
         if i % 2 == 0:
-            print()
+            print("\n")
+        else:
+            print(":")
         i += 1
 
 def main():
 
-    language = ""
-    while language != "en" and language != "fr":
-        language = input("Type \"en\" if you want to translate from French into English, or \"fr\" if you want to translate from English into French:")
+
+    print("Hello, welcome to the translator. Translator supports:")
+    for i in range(len(languages)):
+        print(i, ". ", languages[i], sep="")
+
+
+    i = -1
+    while True:
+        language = input("Type the number of your language:")
+        try:
+            i = int(language)
+            if i > 0 and i < len(languages) - 1:
+                break
+        except:
+            pass
+        print("bad input! try again")
+
+
+    j = -1
+    while True:
+        language = input("Type the number of language you want to translate to:")
+        try:
+            j = int(language)
+            if j > 0 and j < len(languages) - 1 and j != i:
+                break
+        except:
+            pass
+        print("bad input! try again")
 
     word = input("Type the word you want to translate:")
-    print("You chose {} as a language to translate {}.".format(language, word))
-
     headers = {'User-Agent': 'Mozilla/5.0'}
-    if language == "en":
-        address = url + fe + word
-    else:
-        address = url + ef + word
-    page = requests.get(address, headers=headers)
+    page = requests.get(url + languages[i].lower() + "-" + languages[j].lower() + "/" + word, headers=headers)
     if page.status_code == 200:
-        display_results(page, language)
+        display_results(page, i, j)
     else:
         print("Something went wrong. Perhaps bad input. Try again.")
         main()
